e0630db98a2872333aa9c7944f23fe2e81cc8944
Sunil Padikar M <64108055+SunilPadikarManjunatha@users.noreply.github.com>
2022-10-04 15:35:28 -0400

    Fix braces

--- src/TabsHeaderItem.tsx
+++ src/TabsHeaderItem.tsx
@@ -119,7 +119,7 @@ export default function TabsHeaderItem({
                 accessibilityElementsHidden={true}
                 importantForAccessibility="no"
                 name={tab.props.icon || ''}
-                style={{ color: activeColor, opacity}}}
+                style={{ color: activeColor, opacity}}
                 size={24}
               />
             </View>
